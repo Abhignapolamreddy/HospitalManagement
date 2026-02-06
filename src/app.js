47d55bf4e5174360f39e9e106d527bd14ab845de
sikindar m <sikindar.m@laderatechnology.com>
2026-02-06 06:48:40 +0000

    patient

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-const express=require('express')
+const express=require('express');
+const userRouter = require('./modules/patient/patient.routes');
 
 const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
+app.use("/hos",userRouter)
 app.get('/',(req,res)=>{
     res.send('Our Hospital API running');
 })
