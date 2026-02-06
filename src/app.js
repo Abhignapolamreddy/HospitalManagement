3ed5abf2f2f48962da3c8e2e689fe9718d73fa16
sikindar m <sikindar.m@laderatechnology.com>
2026-02-06 12:58:01 +0000

    patient_modules

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ const authRoutes = require("./modules/auth/auth.routes");
 //console.log(process.env.JWT_SECRET)
  
 app.use("/api/auth", authRoutes);
- 
-// app.get('/',(req,res)=>{
-//     res.send('Our Hospital API running');
-// })
+app.use("/hos",userRouter)
+  app.get('/',(req,res)=>{
+    res.send('Our Hospital API running');
+ })
 
 module.exports=app;
