3d48888ca4715e17681ccba7d45c48dad7fa178b
Abhigna Polamreddy <polamreddy.abhigna@laderatechnology.com>
2026-02-07 07:35:36 +0000

    mongo

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express=require('express')
 const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
+console.log(process.env.JWT_SECRET)
 app.get('/',(req,res)=>{
     res.send('Our Hospital API running');
 })
